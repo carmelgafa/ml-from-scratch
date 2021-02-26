dfaf5822d4b2d66c23646c9410afedd86b48ae4d
carmelgafa <carmelgafa@gmail.com>
2021-02-26 21:11:47 +0100

    added logisitc regression

---
+++ logistic_regression/binaryclassification.py
@@ -0,0 +1,143 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z - A scalar or numpy vector
+
+    Returns:
+    s - the sigmoid of z
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+def initialize_with_zeros(dim):
+    """
+    Creates a vector for W initialized to 0 and creates b, initialized to 0
+
+    Arguments:
+    dim - the size of the weight vector, or the number of features to the system
+
+    Returns:
+    w - Initialized weight vecor of shape(dim,1)
+    b - initialized bias, scalar
+    """
+    w = np.zeros((dim, 1))
+    b = 0
+
+    assert w.shape == (dim, 1)
+    assert isinstance(b, float) or isinstance(b, int)
+
+    return w, b
+
+def propagate(w, b, X, Y):
+    """
+    Implement feed forward step, calculate cost function and its gradient
+    
+    Arguments:
+    w - weights, a numpy array. In the image case it will be of shape (num_px * num_px * 3, 1)
+    b - the scalar bis to the neuron
+    X - input data with shape (num_px * num_px * 3, number fo examples)
+    Y - 'true' label vecor size (1, number of examples)
+
+    Returns:
+    Cost - negative log likelihood cost of logistic regression
+    dw - derivative of cost w.r.t. w; same shape of w
+    db - derivative of cost w.r.t. b; same shape of b
+    """
+
+    number_of_examples = X.shape[1]
+
+    #forward propagation
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = np.sum(-(Y * np.log(A) + ((1-Y) * np.log(1-A))), axis=1)/ number_of_examples
+
+    #backward propagation
+    dw = np.dot(X, (A-Y).T) / number_of_examples
+    db = np.sum(A-Y) / number_of_examples
+
+    assert dw.shape == w.shape
+    assert db.dtype == float
+
+    cost = np.squeeze(cost)
+
+    assert cost.shape == ()
+
+    grads = {'dw' : dw,
+             'db' : db}
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+    """
+    optimizes w and b using gradient descent
+
+    Argument:
+    w - weights, numpy array of shape (number _of_input_features, 1)
+    b - bias, scalar
+    X - data numpy array of shape (num_px*num_px*3, number_of_examples)
+    Y - 'true' label vector, numpy array of shape (1, number_of_examples)
+    num_iterations - number of iterations of g.d.
+    learning_rate - learning rate of g.d.
+    print_cost - print the loss every 100 steps
+
+    Returns:
+    params - dictionary containing w and b
+    grads - dictionary containing dw and db
+    costs - list of all the costs computed
+    """
+    costs = []
+
+    for i in range(num_iterations):
+
+        grads, cost = propagate(w, b, X, Y)
+
+        dw = grads['dw']
+        db = grads['db']
+
+        w = w - (learning_rate * dw)
+        b = b - (learning_rate * db)
+
+        if i%100 == 0:
+            costs.append(cost)
+            if print_cost == True:
+                print('print cost after iteration {}: {}'.format(i, cost))
+
+    params = {'w': w, 'b': b}
+    grads = {'dw': dw, 'db': db}
+
+    return params, grads, costs
+
+def predict(w, b, X):
+    """
+    predict label 0 or 1 using learned linera rlogistic regression parameters
+
+    Arguments:
+    w - weights, numpy array of shape (number _of_input_features, 1)
+    b - bias, scalar
+    X - data numpy array of shape (num_px*num_px*3, number_of_examples)
+
+    Returns:
+    Y_prediction - numpy array containg predictions for X
+    """
+    number_of_examples = X.shape[1]
+
+    Y_prediction = np.zeros((1, number_of_examples))
+
+    w = w.reshape(X.shape[0], 1)
+
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(A.shape[1]):
+        if A[0,i] >= 0.5:
+            Y_prediction[0,i] = 1
+        else:
+            Y_prediction[0,i] = 0
+
+    return Y_prediction
---
+++ logistic_regression/exec.py
@@ -0,0 +1,61 @@
+from binaryclassification import *
+from lr_utils import *
+
+def load_data_test():
+    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+    index = 26
+    plt.imshow(train_set_x_orig[index])
+
+    # use instead if imshow
+    image_data = train_set_x_orig[index, :, :, :]
+    img = Image.fromarray(image_data, 'RGB')
+    #img.show()
+
+    print(classes[train_set_y[:, index]])
+    print(classes[np.squeeze(train_set_y[:, index])].decode('utf-8'))
+
+def sigmoid_function_test():
+
+    # test sigmoid function
+    print('sigmoid of [0, 2] is {}'.format(sigmoid(np.array([0, 2]))))
+
+    dim = 5
+    w, b = initialize_with_zeros(dim)
+    print('w = {}'.format(w))
+    print('b = {}'.format(b))
+
+def feedforward_test():
+    w = np.array([[1.], [2.]])
+    b = 2.
+    X = np.array([[1., 2., -1.],[3., 4., -3.2]])
+    Y= np.array([[1, 0, 1]])
+
+    grads, cost = propagate(w, b, X, Y)
+
+    print('dw= {}'.format(grads['dw']))
+    print('db= {}'.format(grads['db']))
+    print('cost= {}'.format(cost))
+
+def optimization_test():
+    w = np.array([[1.], [2.]])
+    b = 2.
+    X = np.array([[1., 2., -1.],[3., 4., -3.2]])
+    Y= np.array([[1, 0, 1]])
+
+    params, grads, costs = optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=True)
+
+    print('w= {}'.format(params['w']))
+    print('b= {}'.format(params['b']))
+    print('dw= {}'.format(grads['dw']))
+    print('db= {}'.format(grads['db']))
+
+def prediction_test():
+    w = np.array([[0.1124579],[0.23106775]])
+    b = -.3
+    X = np.array([[1., -1.1, -3.2],[1.2, 2., 0.1]])
+    print('predictions={}'.format(predict(w, b, X)))
+
+prediction_test()
+#optimization_test()
+#feedforward_test()
