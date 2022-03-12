c564a529ab589f2d76ae7c9905beb81d3a9c98f8
carmelgafa <carmelgafa@gmail.com>
2022-03-12 14:50:09 +0100

    twofeature_batch_gd - returning cost and number of epochs for completion@

--- algorithms/linear_regression/batch_gradient_descent/twofeature_batch_gd.py
+++ algorithms/linear_regression/batch_gradient_descent/twofeature_batch_gd.py
@@ -6,53 +6,48 @@ import pandas as pd
 import sys
 
 def two_feature_gradient_descent(filename, alpha=0.0023, epochs_threshold=100000, costdifference_threshold=0.00001, plot=False):
+    '''
+    Batched gradient descent for a two feature linear regression problem.
+    This algorithm does not use any vectorization
+    '''
+
+    # initialize the coefficients
     a0 = 5
     a1 = 3
     a2 = 1
 
     data_set = None
-
-    epochs_threshold = 100000
-
-
     data_set = pd.read_csv(filename, delimiter=',', header=0, index_col=False)
-
     m = len(data_set)
     epoch = 0
 
     previous_cost = sys.float_info.max
-    
-    costs = []
-    a_1s = []
-    
+
     while True:
         # calculate the hypothesis function for all training data
         data_set['y_hat'] = a0 + (a1 * data_set['x1']) + (a2 * data_set['x2'])
-                    
+
         # calculate the difference between the hypothesis function and the
         # actual y value for all training data
         data_set['y_hat-y'] = data_set['y_hat'] - data_set['y']
-        
+
         # multiply the difference by the x value for all training data
         data_set['y-hat-y.x1'] = data_set['y_hat-y'] * data_set['x1']
         data_set['y-hat-y.x2'] = data_set['y_hat-y'] * data_set['x2']
-        
+
         # square the difference for all training data
         data_set['y-hat-y_sq'] = data_set['y_hat-y'] ** 2
-        
+
         # update the a0 and a1 values
         a0 -= (alpha * (1/m) * sum(data_set['y_hat-y']))
         a1 -= (alpha * (1/m) * sum(data_set['y-hat-y.x1']))
         a2 -= (alpha * (1/m) * sum(data_set['y-hat-y.x2']))
-        
+
         # calculate the cost function
         cost = sum(data_set['y-hat-y_sq']) / (2 * m)
         epoch += 1
 
-        # record the cost and a1 values for plotting
-        costs.append(cost)
-        a_1s.append(a1)
-        
+        # check if the cost function has converged
         cost_difference = previous_cost - cost
         # print(f'Epoch: {epoch}, cost: {cost:.3f}, difference: {cost_difference:.6f}')
         previous_cost = cost
@@ -61,13 +56,13 @@ def two_feature_gradient_descent(filename, alpha=0.0023, epochs_threshold=100000
         if cost_difference < 0:
             print(f'Cost function is diverging. Stopping training.')
             break
-        
+
         # check if the cost function is close enough to 0, if so, break or if the number of 
         # iterations is greater than the threshold, break
         if abs(cost_difference) < costdifference_threshold or epoch > epochs_threshold:
             break
 
-    return a0, a1, a2
+    return a0, a1, a2, epoch, cost
 
 if __name__ == '__main__':
 
@@ -80,6 +75,6 @@ if __name__ == '__main__':
     plot = False
 
     start = timer()
-    a0, a1, a2 = two_feature_gradient_descent(filename, alpha, epochs_threshold, costdifference_threshold, plot)
+    a0, a1, a2, epochs, cost = two_feature_gradient_descent(filename, alpha, epochs_threshold, costdifference_threshold, plot)
     end = timer()
-    print(f'Time: {end - start}, a0: {a0}, a1: {a1}, a2: {a2}')
+    print(f'Time: {end - start}, a0: {a0}, a1: {a1}, a2: {a2} epochs: {epochs}, cost: {cost}')
