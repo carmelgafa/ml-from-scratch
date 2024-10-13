90a6ebd68a3053c8cf83f58cdaf763ac02b4a1d3
Carmel Gafa <carmelgafa@gmail.com>
2024-10-13 19:15:00 +0200

    tweaked image loading

--- ml_algorithms/src/algorithms/cnn/image_convolution.py
+++ ml_algorithms/src/algorithms/cnn/image_convolution.py
@@ -1,27 +1,43 @@
-
+''' image manipulation '''
 #pylint: disable = E0401
 import os
-import cv2
 import matplotlib.pyplot as plt
 
-def process_image(image_path):
+
+def load_image_to_rgb(image_path):
+    '''load and convert image to RGB'''
+
+    image = plt.imread(image_path)
+
+    b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2] # For RGB image
+
+    return b, g, r
+
+def load_image_to_grayscale(image_path):
     '''load and convert image to grayscale'''
 
-    image_path = os.path.join(os.path.dirname(__file__), image_path)
     image = plt.imread(image_path)
 
     # convert image to grayscale
     image = image.mean(axis=2)
 
-    plt.imshow(image, cmap='gray')
-    plt.show()
     return image
 
-
-
 if __name__ == '__main__':
-    bud_path = 'bud.jpg'
+    BUD_FILENAME = 'bud.jpg'
+    bud_path = os.path.join(os.path.dirname(__file__), BUD_FILENAME)
 
-    bud_image = process_image(bud_path)
-    cv2.imshow('image', bud_image)
-    cv2.waitKey(0)
+
+    bud_image_grayscale = load_image_to_grayscale(bud_path)
+    plt.imshow(bud_image_grayscale, cmap='gray')
+    plt.show()
+
+
+    bud_image_r, bud_image_g, bud_image_b = load_image_to_rgb(bud_path)
+    plt.imshow(bud_image_r, cmap='Reds')
+    plt.show()
+    plt.imshow(bud_image_g, cmap='Greens')
+    plt.show()
+    plt.imshow(bud_image_b, cmap='Blues')
+    plt.show()
+        
